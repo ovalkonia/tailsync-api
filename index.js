2769c24b6d413abcdd1c79be5f3b85df4a56277b
RofaBR <rostislavbrigine@gmail.com>
2025-11-11 00:11:32 +0200

    feat: enable CORS

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ const app = express();
 
 // Global middlewares
 
+// CORS middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookie_parser());
