a34a19e5b2d390ea491baedad3d436561c672e59
Mykola Kardash <60359793+ovalkonia@users.noreply.github.com>
2025-11-11 00:25:17 +0200

    chore: final stuff

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
-    if (req.method === 'OPTIONS') {
+    if (req.method === "OPTIONS") {
         return res.sendStatus(200);
     }
 
